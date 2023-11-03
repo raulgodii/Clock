6b158591bbc5e7d9d5ec3880272c97a161e89e7f
raulgodii <raulgodii13@gmail.com>
2023-11-03 08:36:30 +0100

    functions

--- script.js
+++ script.js
@@ -3,4 +3,20 @@ window.onload = () => {
     startClock = document.getElementById("start");
     stopClock = document.getElementById("stop");
     resetClock = document.getElementById("reset");
+
+    startClock.addEventListener("click", startFunc);
+    stopClock.addEventListener("click", stopFunc);
+    resetClock.addEventListener("click", resetFunc);
+}
+
+function startFunc(){
+
+}
+
+function stopFunc(){
+    
+}
+
+function resetFunc(){
+    
 }
