77404a0b411550956e9c9aca15542d697474db5c
raulgodii <raulgodii13@gmail.com>
2023-11-03 08:31:03 +0100

    update

---
+++ script.js
@@ -0,0 +1,6 @@
+window.onload = () => {
+    clock = document.getElementById("clock");
+    startClock = document.getElementById("start");
+    stopClock = document.getElementById("stop");
+    resetClock = document.getElementById("reset");
+}
